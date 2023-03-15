0595f5435bd1e8dfeb9bbdc02280e64e2d33c2ea
JeanieV <venterjeaniebs@gmail.com>
2023-03-15 15:55:28 +0200

    Update 5
    Final Update to User Registration App

--- user-registration-challenge/src/js/user-register.js
+++ user-registration-challenge/src/js/user-register.js
@@ -18,9 +18,8 @@ const registerButton = () => {
     registerTab[0].style.display = "none";
     userTabs[1].style.display = "block";
     registerTab[1].style.display = "none";
-}
+};
 document.getElementById("register-tab-button").addEventListener("click", registerButton);
-
 
 //'Register User' button
 const viewUserButton = () => {
@@ -28,9 +27,8 @@ const viewUserButton = () => {
     registerTab[0].style.display = "block";
     userTabs[1].style.display = "none";
     registerTab[1].style.display = "block";
-}
+};
 document.getElementById("users-tab-button").addEventListener("click", viewUserButton);
-
 
 //Register button shows user list
 const submitButton = document.getElementById("register-submit");
@@ -40,7 +38,7 @@ const registerSubmit = (event) => {
     registerTab[0].style.display = "none";
     userTabs[1].style.display = "block";
     registerTab[1].style.display = "none";
-}
+};
 submitButton.addEventListener("click", registerSubmit);
 
 
@@ -56,91 +54,82 @@ function createUser(event) {
     let email1 = document.getElementById("email").value;
     let password1 = document.getElementById("password").value;
 
-    const newUser = {
-        firstname: firstname1,
-        lastname: lastname1,
-        username: username1,
-        email: email1,
-        password: password1
-    }
 
-    users.push(newUser);
-    populateUserList(users);
-    
     if (firstname1 !== "") {
-
         if (lastname1 == "") {
             alert("Kindly enter your Last Name!");
-    
-        }
-        else if (username1 == "") {
+        } else if (username1 == "") {
             alert("Kindly enter your Username!");
-        }
-        else if (email1 == "") {
-            alert("Kindly enter your email!");
-        }
-        else if (password1 == "") {
+        } else if (email1 == "") {
+            alert("");
+        } else if (password1 == "") {
             alert("Kindly enter a password!");
+        } else {
+            const newUser = {
+                firstname: firstname1,
+                lastname: lastname1,
+                username: username1,
+                email: email1,
+                password: password1,
+            };
+
+            users.push(newUser);
+            populateUserList(users);
         }
-       
-    }
-    else{
+    } else {
         alert("Kindly enter your Registration Information ");
     }
 }
 
 submitButton.addEventListener("click", createUser);
 
-
-
 //second function
-function createUserElement(userparam) {
-    let user1 =
-        `
-            <li class="entry" ondblclick="removeUser(this)">
-                    <span> ${userparam.username} </span>
-                    <span> ${userparam.firstname} </span>
-                    <span> ${userparam.lastname} </span>
-                    <span> ${userparam.email} </span>
-                    <span> ${userparam.password} </span>
-            </li>
-    `
+function createUserElement(userParam, index) {
+    let user1 = `
+      <li class="entry" data-index="${index}">
+        <span> ${userParam.username} </span>
+        <span> ${userParam.firstname} </span>
+        <span> ${userParam.lastname} </span>
+        <span> ${userParam.email} </span>
+        <span> ${userParam.password} </span>
+      </li>
+    `;
     return user1;
 }
 
-
-//third function
 function populateUserList(usersparam) {
     let listElements = document.getElementsByClassName("entry");
+
     let elementLength = listElements.length;
+    
 
-    if (users.length == 0) {
+    Array.from(listElements).forEach((el) => {
+        el.remove();
+    })
+
+    for (let i = 0; i < usersparam.length; i++) {
         
-        for (let i = 0; i < elementLength; i++) {
-            listElements[i].remove();
-        }
+        let user1 = createUserElement(usersparam[i], i);
+        document.getElementById("user-list").innerHTML += user1;
+        console.log(user1);
     }
-    else {
-        for (let i = elementLength; i < usersparam.length; i++) {
-            let user1 = createUserElement(usersparam[i]);
-            document.getElementById("user-list").innerHTML += user1;
-            console.log(user1);
-        }
-    }
+
+    // Update data-index attributes
+    listElements = document.getElementsByClassName("entry");
+    Array.from(listElements).forEach((el, index) => {
+        el.setAttribute("data-index", index);
+        el.addEventListener("dblclick", (event) => {
+            removeUser(event, index);
+        });
+    });
 }
 
 
-function removeUser(listParam) {
-
-    let check = listParam.children[3].innerHTML.trim();
-
-    for (let i = 0; i < users.length; i++) {
-
-        if (users[i].email == check) {
-            users.splice(i, 1);
-        }
-        else { }
-    }
+function removeUser(event, index) {
+    const entry = event.target.parentNode;
+    users.splice(index, 1);
+    entry.parentNode.removeChild(entry);
     populateUserList(users);
-}
 
+    console.log(users);
+}
